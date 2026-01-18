99e86d0667d4b1956a88642254510ac1cb5bf018
Juan Hernandez <195817920+JuanDavH@users.noreply.github.com>
2026-01-18 16:42:11 -0500

    Create basic model for novels

---
+++ src/main/java/com/github/juandavh/webnoveltracker/novel/Novel.java
@@ -0,0 +1,82 @@
+package com.github.juandavh.webnoveltracker.novel;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Novel {
+
+    private final UUID id;
+    private String title;
+    private String author;
+    private String description;
+    private int totalChapters;
+    private final LocalDateTime createdAt;
+
+    public Novel() {
+        this.id = UUID.randomUUID();
+        createdAt = null;
+    }
+
+    public Novel(UUID id, String title, String author, String description, int totalChapters) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.totalChapters = totalChapters;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTotalChapters() {
+        return totalChapters;
+    }
+
+    public void setTotalChapters(int totalChapters) {
+        this.totalChapters = totalChapters;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Novel novel = (Novel) o;
+        return totalChapters == novel.totalChapters && Objects.equals(id, novel.id) && Objects.equals(title, novel.title) && Objects.equals(author, novel.author) && Objects.equals(description, novel.description) && Objects.equals(createdAt, novel.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, description, totalChapters, createdAt);
+    }
+}
