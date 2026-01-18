b73b3ee847c70c0cc406dfd98523eb1bca7a9340
Juan Hernandez <195817920+JuanDavH@users.noreply.github.com>
2026-01-18 16:11:21 -0500

    Initialize project

---
+++ src/main/java/com/github/juandavh/webnoveltracker/WebNovelTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.juandavh.webnoveltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNovelTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebNovelTrackerApplication.class, args);
+    }
+
+}
