cc1bb362e82f75ceb4f3a7b16ec36c6ebe400c6c
Juan Hernandez <195817920+JuanDavH@users.noreply.github.com>
2026-01-21 22:45:35 -0500

    Create NovelFolder, FolderItem entities with corresponding database tables

---
+++ src/main/java/com/github/juandavh/webnoveltracker/novelfolder/FolderItem.java
@@ -0,0 +1,86 @@
+package com.github.juandavh.webnoveltracker.novelfolder;
+
+import com.github.juandavh.webnoveltracker.novel.Novel;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "folderitems")
+public class FolderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    // Many-to-one relationship to allow multiple folders to contain the same novel
+    @ManyToOne
+    @JoinColumn(name="novelID", nullable = false)
+    private Novel novel;
+
+    // Many-to-one relationship to allow folders to contain multiple folder items
+    @ManyToOne
+    @JoinColumn(name="folderID", nullable = false)
+    private NovelFolder folder;
+
+    private int position;
+    private boolean pinned;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    public FolderItem() {
+
+    }
+
+    public FolderItem(Novel novel, NovelFolder folder, int position, boolean pinned) {
+        this.novel = novel;
+        this.folder = folder;
+        this.position = position;
+        this.pinned = pinned;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Novel getNovel() {
+        return novel;
+    }
+
+    public void setNovel(Novel novel) {
+        this.novel = novel;
+    }
+
+    public NovelFolder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(NovelFolder folder) {
+        this.folder = folder;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public boolean isPinned() {
+        return pinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        this.pinned = pinned;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+}
---
+++ src/main/java/com/github/juandavh/webnoveltracker/novelfolder/NovelFolder.java
@@ -0,0 +1,68 @@
+package com.github.juandavh.webnoveltracker.novelfolder;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name="novelfolders")
+public class NovelFolder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String folderName;
+
+    @OneToMany(mappedBy = "folder", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<FolderItem> items;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    public NovelFolder() {
+
+    }
+
+    public NovelFolder(String folderName, List<FolderItem> items) {
+        this.folderName = folderName;
+        this.items = items;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFolderName() {
+        return folderName;
+    }
+
+    public void setFolderName(String folderName) {
+        this.folderName = folderName;
+    }
+
+    public List<FolderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<FolderItem> items) {
+        this.items = items;
+    }
+
+    public void addItem(FolderItem item) {
+        items.add(item);
+        item.setFolder(this);
+    }
+
+    public void removeItem(FolderItem item) {
+        items.remove(item);
+        item.setFolder(null);
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
