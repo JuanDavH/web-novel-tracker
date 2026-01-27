a80a71c4ade4a8c120e36e0e3233d483b763a2fb
Juan Hernandez <195817920+JuanDavH@users.noreply.github.com>
2026-01-26 23:11:53 -0500

    Fix message format for InvalidFolderItemPositionException

--- src/main/java/com/github/juandavh/webnoveltracker/novelfolder/InvalidFolderItemPositionException.java
+++ src/main/java/com/github/juandavh/webnoveltracker/novelfolder/InvalidFolderItemPositionException.java
@@ -2,6 +2,6 @@ package com.github.juandavh.webnoveltracker.novelfolder;
 
 public class InvalidFolderItemPositionException extends RuntimeException {
     public InvalidFolderItemPositionException(int newPosition, int maxPosition) {
-        super("Position" + newPosition + "is out of bounds: must be between 0 and " + maxPosition);
+        super("Position " + newPosition + " is out of bounds: must be between 0 and " + maxPosition);
     }
 }
